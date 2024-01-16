339814a2999c8e005e34e2d28680d4f810094824
Caleb Leedy <leedy.caleb@gmail.com>
2024-01-16 14:36:13 -0600

    Updated my response to Dr. Fuller in which I compared parametric and semiparametric estimators to understand their efficiency.

--- Simulations/T3Assess_Semiparametric_Cost/compare_algs.R
+++ Simulations/T3Assess_Semiparametric_Cost/compare_algs.R
@@ -44,53 +44,88 @@ min_var_alg <-
   }
 
   # We use the population functional forms for gamma_hat and v_gamma
-  g_11 <- mean(df$delta_00 / df$prob_00 * (theta + df$X))
-  g_21 <- mean(df$delta_10 / df$prob_10 * (theta + df$X))
-  g_22 <- mean(df$delta_10 / df$prob_10 * (theta + df$X + cov_e1e2 * (df$Y1 - df$X)))
-  g_31 <- mean(df$delta_01 / df$prob_01 * (theta + df$X))
-  g_33 <- mean(df$delta_01 / df$prob_01 * (df$Y2))
-  g_41 <- mean(df$delta_11 / df$prob_11 * (theta + df$X))
-  g_42 <- mean(df$delta_11 / df$prob_11 * (theta + df$X + cov_e1e2 * (df$Y1 - df$X)))
-  g_43 <- mean(df$delta_11 / df$prob_11 * (df$Y2))
-  g_44 <- mean(df$delta_11 / df$prob_11 * (df$Y2))
+  if (gfun == "Y2") {
+
+    Eg <- theta
+    Egx <- theta + df$X 
+    Egxy1 <- theta + df$X + cov_e1e2 * (df$Y1 - df$X)
+    Egxy2 <- df$Y2
+    EEg2 <- theta^2 + 2
+    EEgx2 <- theta^2 + 1
+    EEgxy12 <- theta^2 + 1 + cov_e1e2^2
+    EEgxy22 <- theta^2 + 2
+    EEgxy1Egxy2 <- theta^2 + 1 + cov_e1e2^2
+
+  } else if (gfun == "Y1^2 * Y2") {
+
+    Eg <- 2 * theta
+    Egx <- df$X^3 + theta * df$X^2 + theta + 2 * df$X * cov_e1e2 + df$X
+    Egxy1 <- df$Y1^2 * (theta + df$X + cov_e1e2 * (df$Y1 - df$X))
+    Egxy2 <- df$Y2 * (df$X^2 + 2 * cov_e1e2 * df$X * (df$Y2 - df$X - theta) + 
+                      1 + cov_e1e2^2 * (df$Y2 - df$X - theta)^2 - cov_e1e2^2)
+    EEg2 <- 12 * (theta^2 + 2 * cov_e1e2^2 + 4 * cov_e1e2 + 4)
+    EEgx2 <- 6 * theta^2 + 4 * cov_e1e2^2 + 16 * cov_e1e2 + 22
+    EEgxy12 <- 12 * (theta^2 + 3 * cov_e1e2^2 + 4 * cov_e1e2 + 3)
+    EEgxy22 <- 
+      6 * theta^2 + 4 * theta^2 * cov_e1e2^2 + 2 * theta^2 * cov_e1e2^4 + 34 +
+      32 * cov_e1e2 + 20 * cov_e1e2^2 + 16 * cov_e1e2^3 + 18 * cov_e1e2^4
+    EEgxy1Egxy2 <- 
+      theta^2 * (6 + 4 * cov_e1e2^2 + 2 * cov_e1e2^4) + 22 + 24 * cov_e1e2 +
+      26 * cov_e1e2^2 + 20 * cov_e1e2^3 + 18 * cov_e1e2^4 + 4 * cov_e1e2^5 +
+      6 * cov_e1e2^6
+
+  } else {
+    stop("We have only implemented two g-functions.")
+  }
+
+  g_11 <- mean(df$delta_00 / df$prob_00 * (Egx))
+  g_21 <- mean(df$delta_10 / df$prob_10 * (Egx))
+  g_22 <- mean(df$delta_10 / df$prob_10 * (Egxy1))
+  g_31 <- mean(df$delta_01 / df$prob_01 * (Egx))
+  g_33 <- mean(df$delta_01 / df$prob_01 * (Egxy2))
+  g_41 <- mean(df$delta_11 / df$prob_11 * (Egx))
+  g_42 <- mean(df$delta_11 / df$prob_11 * (Egxy1))
+  g_43 <- mean(df$delta_11 / df$prob_11 * (Egxy2))
+  g_44 <- mean(df$delta_11 / df$prob_11 * (df$g_i))
 
   gam_vec <- c(g_11, g_21, g_22, g_31, g_33, g_41, g_42, g_43, g_44)
 
-  v_gam <- matrix(rep(0, 81), nrow = 9)
-  v_gam[1, 1] <- 1 / df$prob_00[1] * (theta^2 + 1) - theta^2
+  v_gam <- matrix(rep(-Eg^2, 81), nrow = 9)
+  v_gam[1, 1] <- 1 / df$prob_00[1] * (EEgx2) - Eg^2
 
-  v_gam[2, 2] <- 1 / df$prob_10[1] * (theta^2 + 1) - theta^2
-  v_gam[2, 3] <- 1 / df$prob_10[1] * (theta^2 + 1) - theta^2
+  v_gam[2, 2] <- 1 / df$prob_10[1] * (EEgx2) - Eg^2
+  v_gam[2, 3] <- 1 / df$prob_10[1] * (EEgx2) - Eg^2
   v_gam[3, 2] <- v_gam[2, 3]
-  v_gam[3, 3] <- 1 / df$prob_10[1] * (theta^2 + 1) - theta^2 
+  v_gam[3, 3] <- 1 / df$prob_10[1] * (EEgxy12) - Eg^2 
 
-  v_gam[4, 4] <- 1 / df$prob_01[1] * (theta^2 + 1) - theta^2
-  v_gam[4, 5] <- 1 / df$prob_01[1] * (theta^2 + 1) - theta^2
+  v_gam[4, 4] <- 1 / df$prob_01[1] * (EEgx2) - Eg^2
+  v_gam[4, 5] <- 1 / df$prob_01[1] * (EEgx2) - Eg^2
   v_gam[5, 4] <- v_gam[4, 5]
-  v_gam[5, 5] <- 1 / df$prob_01[1] * (theta^2 + 2) - theta^2 
+  v_gam[5, 5] <- 1 / df$prob_01[1] * (EEgxy22) - Eg^2 
 
-  v_gam[6, 6] <- 1 / df$prob_11[1] * (theta^2 + 1) - theta^2
-  v_gam[6, 7] <- 1 / df$prob_11[1] * (theta^2 + 1) - theta^2
+  v_gam[6, 6] <- 1 / df$prob_11[1] * (EEgx2) - Eg^2
+  v_gam[6, 7] <- 1 / df$prob_11[1] * (EEgx2) - Eg^2
   v_gam[7, 6] <- v_gam[6, 7] 
-  v_gam[6, 8] <- 1 / df$prob_11[1] * (theta^2 + 1) - theta^2
+  v_gam[6, 8] <- 1 / df$prob_11[1] * (EEgx2) - Eg^2
   v_gam[8, 6] <- v_gam[6, 8]
-  v_gam[6, 9] <- 1 / df$prob_11[1] * (theta^2 + 1) - theta^2
+  v_gam[6, 9] <- 1 / df$prob_11[1] * (EEgx2) - Eg^2
   v_gam[9, 6] <- v_gam[6, 9]
-  v_gam[7, 7] <- 1 / df$prob_11[1] * (theta^2 + 1) - theta^2
-  v_gam[7, 8] <- 1 / df$prob_11[1] * (theta^2 + 1) - theta^2
+  v_gam[7, 7] <- 1 / df$prob_11[1] * (EEgxy12) - Eg^2
+  v_gam[7, 8] <- 1 / df$prob_11[1] * (EEgxy1Egxy2) - Eg^2
   v_gam[8, 7] <- v_gam[7, 8] 
-  v_gam[7, 9] <- 1 / df$prob_11[1] * (theta^2 + 1) - theta^2
+  v_gam[7, 9] <- 1 / df$prob_11[1] * (EEgxy12) - Eg^2
   v_gam[9, 7] <- v_gam[7, 9] 
-  v_gam[8, 8] <- 1 / df$prob_11[1] * (theta^2 + 2) - theta^2
-  v_gam[8, 9] <- 1 / df$prob_11[1] * (theta^2 + 2) - theta^2
+  v_gam[8, 8] <- 1 / df$prob_11[1] * (EEgxy22) - Eg^2
+  v_gam[8, 9] <- 1 / df$prob_11[1] * (EEgxy22) - Eg^2
   v_gam[9, 8] <- v_gam[8, 9]
-  v_gam[9, 9] <- 1 / df$prob_11[1] * (theta^2 + 2) - theta^2
+  v_gam[9, 9] <- 1 / df$prob_11[1] * (EEg2) - Eg^2
 
   v_gam <- v_gam / nrow(df)
 
   # CVXR
   c_hat <- Variable(length(gam_vec))
 
+  # TODO: We can change the inequality constraints to equality constraints.
   if (mod == "para") {
     con1 <- matrix(rep(1, length(gam_vec)), nrow = 1) %*% c_hat <= 1
     con2 <- matrix(rep(1, length(gam_vec)), nrow = 1) %*% c_hat >= 1
--- Simulations/T3Assess_Semiparametric_Cost/semi_cost.R
+++ Simulations/T3Assess_Semiparametric_Cost/semi_cost.R
@@ -30,6 +30,10 @@ source("Simulations/T3Assess_Semiparametric_Cost/compare_algs.R")
 # * Monte Carlo Simulations *
 # ***************************
 
+# ************************
+# * theta = E[g] = E[Y2] *
+# ************************
+
 clust <- parallel::makeCluster(min(parallelly::availableCores() - 2, 100))
 registerDoParallel(clust)
 
@@ -44,7 +48,8 @@ mc_theta <-
           .packages = c("dplyr", "stringr", "CVXR")) %dorng% {
 
     # Generate Data
-    df <- gen_optsim_data(n = n_obs, theta = true_theta, cov_e1e2 = cov_e1e2)
+    # df <- gen_optsim_data(n = n_obs, theta = true_theta, cov_e1e2 = cov_e1e2)
+    df <- gen_unbal_data(n = n_obs, theta = true_theta, cov_e1e2 = cov_e1e2)
 
     # Get Estimates
     oracle_est <- mean(df$Y2)
@@ -175,31 +180,147 @@ mc_theta |>
                caption = paste0("True Theta is ", true_theta,
                                 ". Cov_e1e2 = ", cov_e1e2 ))
 
-# * Testing the standard deviations
-# When sigma_11 = 1 and sigma_22 = 1, we have:
-# Expected sd: 
-# Oracle:
-sqrt(2 / n_obs)
-# OracleX: 
-sqrt(1 / n_obs)
-# CC:
-sqrt(2 / (n_obs * 0.6))
-# IPW:
-sqrt((2 + (0.6 * true_theta^2)) / (n_obs * 0.4))
-# WLS:
-m_mat <- matrix(c(1, 0, 1, 0, 0, 1, 0, 1), ncol = 2)
-v_mat <- matrix(c(1 / (n_obs * 0.4), cov_e1e2 / (n_obs * 0.4), 0, 0,
-                  cov_e1e2 / (n_obs * 0.4), 1 / (n_obs * 0.4), 0, 0,
-                  0, 0, 1 / (n_obs * 0.2), 0,
-                  0, 0, 0, 1 / (n_obs * 0.2)), nrow = 4)
-exp_inv_cov <- solve(t(m_mat) %*% solve(v_mat) %*% m_mat)
-sqrt(exp_inv_cov[2, 2])
-# Prop
-exp_var <- 
-  (1 + cov_e1e2^2 * (1 / 0.4 - 1 / 0.6) + 1 / 0.6 + 
-   2 * cov_e1e2^2 * (1 / 0.6 - 1) * 0.4 / 0.6) / n_obs
+# *******************************
+# * theta = E[g] = E[Y1^2 * Y2] *
+# *******************************
 
-sqrt(exp_var)
-# Semiopt
-## Default
-sqrt((6 + 7.5 * cov_e1e2^2) / n_obs)
+clust <- parallel::makeCluster(min(parallelly::availableCores() - 2, 100))
+registerDoParallel(clust)
+
+mc_theta <-
+  foreach(iter = 1:B,
+          .options.RNG = 1,
+          .packages = c("dplyr", "stringr", "CVXR")) %dorng% {
+
+    # g_fun <- "Y1^2Y2"
+    
+    # Generate Data
+    # df <- gen_optsim_data(n = n_obs, theta = true_theta, cov_e1e2 = cov_e1e2)
+    df <- gen_unbal_data(n = n_obs, theta = true_theta, cov_e1e2 = cov_e1e2)
+
+    # Get Estimates
+    oracle_est <- mean(df$Y1^2 * df$Y2)
+    oraclex_est <- mean((df$Y1)^2 * (df$Y2 - df$X))
+    ipw_est <- 
+      filter(df, delta_1 == 1, delta_2 == 1) |>
+      mutate(g_f = Y1^2 * Y2) |>
+      mutate(g_est = g_f / prob_11) |>
+      pull(g_est) |>
+      sum() / nrow(df)
+    cc_est <- 
+      filter(df, delta_1 == 1, delta_2 == 1) |>
+      mutate(g_f = Y1^2 * Y2) |>
+      pull(g_f) |>
+      mean()
+    wls_est <- comb_lin_est(df, gfun = "Y1^2 * Y2", cov_e1e2 = cov_e1e2)
+    wlstt_est <- 
+      comb_lin_est(df, gfun = "Y1^2 * Y2", cov_e1e2 = cov_e1e2, theta2 = true_theta)
+    prop_est <- prop_nmono_est(df, gfun = "Y1^2 * Y2", pow = 3)
+    propopt_est <- opt_theta_c(df, gfun = "Y1^2 * Y2", pow = 3)
+    semiopt_est <- opt_semi_est(df, gfun = "Y1^2 * Y2", pow = 3)
+    semidef_est <- opt_semi_est(df, gfun = "Y1^2 * Y2", est = "default", pow = 3)
+    semidel_est <- opt_delta_c(df, gfun = "Y1^2 * Y2", pow = 3)
+    semideldef_est <- opt_delta_c(df, gfun = "Y1^2 * Y2", est = "default", pow = 3)
+
+    conalg_para <- 
+      min_var_alg(df, gfun = "Y1^2 * Y2", mod = "para", cov_e1e2 = cov_e1e2)
+    conalg_sumzero <- 
+      min_var_alg(df, gfun = "Y1^2 * Y2", mod = "sumzero", cov_e1e2 = cov_e1e2)
+    conalg_outrob <- 
+      min_var_alg(df, gfun = "Y1^2 * Y2", mod = "out_rob", cov_e1e2 = cov_e1e2)
+    conalg_resprob <- 
+      min_var_alg(df, gfun = "Y1^2 * Y2", mod = "resp_rob", cov_e1e2 = cov_e1e2)
+    conalg_doubrob <- 
+      min_var_alg(df, gfun = "Y1^2 * Y2", mod = "double_rob", cov_e1e2 = cov_e1e2)
+
+    return(tibble(oracle = oracle_est,
+                  oraclex = oraclex_est,
+                  cc = cc_est,
+                  ipw = ipw_est,
+                  wls = wls_est,
+                  wlstt = wlstt_est,
+                  prop = prop_est,
+                  propopt = propopt_est,
+                  semiopt = semiopt_est,
+                  semidef = semidef_est,
+                  semidel = semidel_est,
+                  semideldef = semideldef_est,
+                  conpara = conalg_para$theta_est,
+                  conzero = conalg_sumzero$theta_est,
+                  conout = conalg_outrob$theta_est,
+                  conresp = conalg_resprob$theta_est,
+                  condoub = conalg_doubrob$theta_est
+    ))
+  } |> 
+  bind_rows()
+
+stopCluster(clust)
+
+# *******************
+# * Analyze Results *
+# *******************
+true_g <- 2 * true_theta
+
+mc_theta |>
+  summarize(
+    bias_oracle = mean(oracle) - true_g,
+    bias_oraclex = mean(oraclex) - true_g,
+    bias_cc = mean(cc) - true_g,
+    bias_ipw = mean(ipw) - true_g,
+    bias_wls = mean(wls) - true_g,
+    bias_wlstt = mean(wlstt) - true_g,
+    bias_prop = mean(prop) - true_g,
+    bias_propopt = mean(propopt) - true_g,
+    bias_semiopt = mean(semiopt) - true_g,
+    bias_semidef = mean(semidef) - true_g,
+    bias_semidel = mean(semidel) - true_g,
+    bias_semideldef = mean(semideldef) - true_g,
+    bias_conpara = mean(conpara) - true_g,
+    bias_conzero = mean(conzero) - true_g,
+    bias_conout = mean(conout) - true_g,
+    bias_conresp = mean(conresp) - true_g,
+    bias_condoub = mean(condoub) - true_g,
+    sd_oracle = sd(oracle),
+    sd_oraclex = sd(oraclex),
+    sd_cc = sd(cc),
+    sd_ipw = sd(ipw),
+    sd_wls = sd(wls),
+    sd_wlstt = sd(wlstt),
+    sd_prop = sd(prop),
+    sd_propopt = sd(propopt),
+    sd_semiopt = sd(semiopt),
+    sd_semidef = sd(semidef),
+    sd_semidel = sd(semidel),
+    sd_semideldef = sd(semideldef),
+    sd_conpara = sd(conpara),
+    sd_conzero = sd(conzero),
+    sd_conout = sd(conout),
+    sd_conresp = sd(conresp),
+    sd_condoub = sd(condoub),
+    tstat_oracle = (mean(oracle) - true_g) / sqrt(var(oracle) / B),
+    tstat_oraclex = (mean(oraclex) - true_g) / sqrt(var(oraclex) / B),
+    tstat_cc = (mean(cc) - true_g) / sqrt(var(cc) / B),
+    tstat_ipw = (mean(ipw) - true_g) / sqrt(var(ipw) / B),
+    tstat_wls = (mean(wls) - true_g) / sqrt(var(wls) / B),
+    tstat_wlstt = (mean(wlstt) - true_g) / sqrt(var(wlstt) / B),
+    tstat_prop = (mean(prop) - true_g) / sqrt(var(prop) / B),
+    tstat_propopt = (mean(propopt) - true_g) / sqrt(var(propopt) / B),
+    tstat_semiopt = (mean(semiopt) - true_g) / sqrt(var(semiopt) / B),
+    tstat_semidef = (mean(semidef) - true_g) / sqrt(var(semidef) / B),
+    tstat_semidel = (mean(semidel) - true_g) / sqrt(var(semidel) / B),
+    tstat_semideldef = (mean(semideldef) - true_g) / sqrt(var(semideldef) / B),
+    tstat_conpara = (mean(conpara) - true_g) / sqrt(var(conpara) / B),
+    tstat_conzero = (mean(conzero) - true_g) / sqrt(var(conzero) / B),
+    tstat_conout = (mean(conout) - true_g) / sqrt(var(conout) / B),
+    tstat_conresp = (mean(conresp) - true_g) / sqrt(var(conresp) / B),
+    tstat_condoub = (mean(condoub) - true_g) / sqrt(var(condoub) / B)
+  ) |>
+  tidyr::pivot_longer(cols = everything(),
+               names_to = c(".value", "algorithm"),
+               names_pattern = "(.*)_(.*)") |>
+  mutate(pval = pt(-abs(tstat), df = B)) |>
+  knitr::kable(#"latex", booktabs = TRUE,
+               digits = 3,
+               caption = paste0("True g is ", true_g,
+                                ". Cov_e1e2 = ", cov_e1e2 ))
+
